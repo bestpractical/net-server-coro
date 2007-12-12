035b8eac4d4bb361cfe81c3e4905d8b539a7f2da
Alex Vandiver <alexmv@bestpractical.com>
2007-12-12 22:58:41 +0000

    Working SSL and multi-listen

--- lib/Net/Server/Coro.pm
+++ lib/Net/Server/Coro.pm
@@ -1,117 +1,95 @@
 package Net::Server::Coro;
 
 use strict;
-use vars qw($VERSION @ISA $LOCK_EX $LOCK_UN);
-use POSIX qw(WNOHANG);
-use Fcntl ();
-use Net::Server ();
-use Net::Server::SIG qw(register_sig check_sigs);
+use warnings;
+use vars qw($VERSION);
 use Coro;
 use Coro::Semaphore;
-use Coro::Event;
+use Coro::Handle;
 use Coro::Socket;
-@ISA = qw(Net::Server);
+use base qw(Net::Server);
 
-### override-able options for this package
-sub options {
-  my $self = shift;
-  my $prop = $self->{server};
-  my $ref  = shift;
+my $connections = new Coro::Semaphore 500;    # $MAX_CONNECTS;
+use vars qw/$SELF @FH @POOL/;
 
-  $self->SUPER::options($ref);
+no warnings 'redefine';
 
-  foreach ( qw(max_servers max_requests request_timeout response_timeout) ){
-    $prop->{$_} = undef unless exists $prop->{$_};
-    $ref->{$_} = \$prop->{$_};
-  }
+sub Coro::Handle::accept {
+    my ( $peername, $fh );
+    while () {
+        ( $fh, $peername ) = tied( ${ $_[0] } )->[0]->accept;
+        if ($peername) {
+            my $socket
+                = $_[0]->new_from_fh( $fh,
+                forward_class => tied( ${ $_[0] } )->[7] );
+            return wantarray ? ( $socket, $peername ) : $socket;
+        }
 
+        return unless $!{EAGAIN};
+
+        $_[0]->readable or return;
+    }
 }
 
-sub Coro::Socket::AUTOLOAD {
-    warn $Coro::Socket::AUTOLOAD;
-}
-sub pre_bind {
-    warn "pre_bind"
-}
-my $SOCK;
-sub bind {
-    warn "bind";
+sub post_bind_hook {
     my $self = shift;
     my $prop = $self->{server};
-  foreach my $port ( @{ $prop->{port} } ){
-    my $obj = $self->proto_object($prop->{host},
-                                  $port,
-                                  $prop->{proto},
-                                  ) || next;
-                                  $SOCK = $obj;
-                                  #push @{ $prop->{sock} }, $obj;
-  }
+    $prop->{sock}
+        = [ map { make_coro_socket($_) } @{ $prop->{sock} } ];
 }
 
-sub proto_object {
-    my $self = shift;
-    my ($host,$port,$proto) = @_;
-    $host = '0.0.0.0' if $host eq '*';
-    return Coro::Socket->new(
-        LocalAddr   => $host,
-        LocalPort   => $port,
-        ReuseAddr   => 1,
-        Listen      => 1,
-    );
+sub make_coro_socket {
+    my $socket = shift;
+    my $proto = $socket->NS_proto;
+    $socket = Coro::Socket->new_from_fh( $socket, forward_class => ref $socket );
+    $socket->NS_proto($proto);
+    return $socket;
 }
 
+sub get_client_info { }
 
-sub post_bind { warn "post_bind: $SOCK" }
-my @fh;
-my @pool;
-my $connections = new Coro::Semaphore 500; # $MAX_CONNECTS;
-
-use vars '$SELF';
 sub handler {
-   while () {
-      my $prop = $SELF->{server};
-      my $fh = pop @fh;
-      if ($fh) {
-        $prop->{connected} = 1;
-        my $from = $fh->peername;
-        warn "CONNECTED! ($fh - $from)";
-        $prop->{client} = $fh;
-        $SELF->run_client_connection;
-        last if $SELF->done;
-        $prop->{connected} = 0;
-         close $fh;
-         $connections->up;
-      } else {
-         last if @pool >= 20; #$MAX_POOL;
-         push @pool, $Coro::current;
-         schedule;
-      }
-   }
+    while (1) {
+        my $prop = $SELF->{server};
+        my $fh   = pop @FH;
+        if ($fh) {
+            $prop->{connected} = 1;
+            my $from = $fh->peername;
+            $prop->{client} = $fh;
+            $SELF->run_client_connection;
+            last if $SELF->done;
+            $prop->{connected} = 0;
+            close $fh;
+            $connections->up;
+        } else {
+            last if @POOL >= 20;    #$MAX_POOL;
+            push @POOL, $Coro::current;
+            schedule;
+        }
+    }
 }
 
 ### prepare for connections
 sub loop {
     my $self = $SELF = shift;
     my $prop = $self->{server};
+    for my $socket ( @{ $prop->{sock} } ) {
+        async {
+            while (1) {
+                $connections->down;
+                my $accepted = scalar $socket->accept;
+                next unless $accepted;
+                push @FH, $accepted;
+                if (@POOL) {
+                    ( pop @POOL )->ready;
+                } else {
+                    async \&handler;
+                }
 
-    async {
-        while (1) {
-            $connections->down;
-            warn "CC";
-            push @fh, scalar $SOCK->accept;
-            warn "GOT: @fh\n";
-            if (@pool) {
-                (pop @pool)->ready;
-            } else {
-                async \&handler;
             }
-
-        }
-    };
+        };
+    }
     schedule;
-    loop;
 }
-
-*FileHandle::peername = *Coro::Socket::peername;
 
 1;
